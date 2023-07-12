d6f85f902e49a167350a1899d53c64de725943eb
aziz2982 <azizbek30052982@gmail.com>
2023-07-13 00:44:10 +0500

    Switch, for, KarraJadval

---
+++ 4-lesson/index.js
@@ -0,0 +1,68 @@
+// Switch
+// if(contation)
+// Switch Case  switch(veraybel) 
+//  case dagi malumotga veraybel togri kelsa qattiy tenglikka : nuqtadan keyingi kodni korsatasi
+// break togri javob chiqsa toxtadi agar break qoymasak kodni davomi dep oylaydi    
+// default shartlarga togri kelmasa shu chiqadi else ga oxshaydi
+// if dan farqi biz switch case da shart kirita olmaymiz faqat === ga togri kelsa yuradi
+    // if ->  <,>,<=,>=,==,===
+    // switch -> ====
+        // Masalan
+        // let data = "salom"; 
+        // switch(data){
+        //     case "qor" :
+        //         console.log("bugun qor");
+        //         break;
+        //     case "yomgir" :
+        //         console.log("bugun yomgir");
+        //         break;
+        //     default :
+        //     console.log("No aniq havo");
+        // }
+
+
+
+// for loop
+// for ozidan 3ta parametr oladi for(boshlanish, manzil, step)
+// agar for ( i = 1; true ; i = i +1) agar ortadagi true bolmasa kodimiz yurmaydi true qaytmaguncha hozirgi kodda chekszlik cheksiz sonchiqadi
+// birinchi qiymatnii ochirip qoysak boladi va kodimiz ishlay veradi agar oxirisidagi malumotni ochirsak log da cheksiz 1 chiqip turadi
+// agar hammasini ochirsak hatolik beradi    
+    // Masalan
+        // let i = 1
+        // for(; i < 10; ){
+        //     console.log(i);
+        // }
+// for ni ichida var yoki let ni farqi var global for ni ichida va tashqarisidaham ishlay veradi let ishlamaydi
+    // Masalan
+        // for(var i = 1; i <10; i++){                  // for(let i = 1; i < 10; i++){
+        // }                                              };
+        // console.log(i);                                  console.log(i);
+
+
+    // for(i=1; i < 10; i= i +1){
+    //     console.log(i);
+    // }
+    // let i = 1
+    // for(i; i < 10; i++){
+    //     console.log(i);
+    // }
+
+// Karra jadval -> num ga hohlagan son kiritamiz va u 10 gacha kopaytrip hisoblab beradi
+    // let num = 5
+    // for( i = 1; i <= 10; i++){
+    //     console.log(`${num} * ${i} = ${num * i}`);
+    // }  
+
+
+
+
+
+    // karra jadval -> 9 gacha bolgan sonlarni karra jadvalini chiqazib beradi
+//     for( i = 1; i <= 9; i++){
+//         console.log(`
+
+// ${i} - lik karra jadvali `);
+//         for(j =1; j <= 9; j++){
+//             console.log(`${i} * ${j} = ${i * j}`);
+//         }
+//     }
